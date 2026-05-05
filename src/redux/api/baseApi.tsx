456c142f434cc6123ce7b1c25b8d510d1c4e8b3f
DevNiloy <anisul2917@gmail.com>
2026-05-06 03:19:43 +0600

    changed

--- src/redux/api/baseApi.tsx
+++ src/redux/api/baseApi.tsx
@@ -1,18 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: `https://api.zayn-bd.com/api`,
+    baseUrl: "https://api.zayn-bd.com/api",
     credentials: "include",
-    prepareHeaders: (headers) => {
-      const token = localStorage.getItem("token");
-      if (token) {
-        headers.set("authorization", `Bearer ${token}`);
-      }
-      return headers;
-    },
   }),
   tagTypes: ["User", "Product", "Order", "Category"],
-  endpoints: () => ({}), // এন্ডপয়েন্টগুলো অন্য ফাইল থেকে ইনজেক্ট করা হবে
+  endpoints: () => ({}),
 });
