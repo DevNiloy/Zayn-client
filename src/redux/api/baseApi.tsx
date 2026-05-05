7dba82a66e2a0b86102da462fafc9cb22efaabf4
DevNiloy <anisul2917@gmail.com>
2026-05-06 05:09:33 +0600

    update

--- src/redux/api/baseApi.tsx
+++ src/redux/api/baseApi.tsx
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5001/api",
+    baseUrl: "https://api.zayn-bd.com/api",
     credentials: "include",
   }),
   tagTypes: ["User", "Product", "Order", "Category"],
